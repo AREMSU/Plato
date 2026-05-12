e5ffcc999b095906e281d34c14ccb920a0ad083f
AREMSU <arnavjungdahala1@gmail.com>
2026-05-12 08:22:17 +0545

    feat: image filter with HuggingFace ViT, QA review endpoint, meal status field

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Meal, Booking
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Meal)
+admin.site.register(Booking)
--- api/ai_urls.py
+++ api/ai_urls.py
@@ -3,4 +3,7 @@ from . import ai_views
 
 urlpatterns = [
     path('recommended/', ai_views.RecommendedMealsView.as_view(), name='recommended-meals'),
+    path('filter-image/', ai_views.ImageFilterView.as_view(), name='filter-image'),
+    path('qa/review/', ai_views.QAReviewView.as_view(), name='qa-review'),
+    path('qa/review/<int:pk>/', ai_views.QAReviewView.as_view(), name='qa-review-detail'),
 ]
