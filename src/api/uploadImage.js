e7379a2c6601358a880f5b69214e444465005149
Aditya Dev Joshi <aditjoshi2005@gmail.com>
2026-05-15 22:26:12 +0545

    Upload images

--- src/api/uploadImage.js
+++ src/api/uploadImage.js
@@ -32,7 +32,7 @@ export const uploadImageToCloudinary = async (localUri) => {
             return null;
         }
     } catch (error) {
-        console.error('Upload failed:', error);
+        console.error('Upload failed:', );
         return null;
     }
 };
